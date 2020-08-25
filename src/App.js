6fbda62d474683c7143650483e62960b6babc725
JKrup <graziano.laveder@gmail.com>
2020-08-25 11:17:50 +0200

    implemented firebase utils, included ability to store authenticated users into cloud firestore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from "react-router-dom"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-up/sign-in-and-up.component"
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,40 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // check if the user is signing in
+      if (userAuth) {
+        // if there is a document in firebase get back the userReference object
+        // if there is no document, a new entry is created in the createUserProf. function
+        // with the data from our userAuth process
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        /**
+         * create a listener on the userRef object to get notified of any changes to it
+         * we also get back the first state of that data
+         * this data from the database, we set equal to our currentUser in our app
+         * so that we can massage the data
+         */
+        userRef.onSnapshot((snapShot) => {
+          /**
+           * the ID and the rest of the data is not stored in the same place
+           * how can we get the data of both places, with one call?
+           * get the ID from snapShot.id
+           * and as a 2nd arg spread ...snapShot.data() where all other props are
+           */
+          this.setState(
+            {
+              currentUser: { id: snapShot.id, ...snapShot.data() },
+            },
+            () => console.log(this.state)
+          )
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
+      console.log(this.state) //cannot be added here, beacuse setState is an async call
+
+      // this.setState({ currentUser: user })
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,17 +13,75 @@ const firebaseConfig = {
   messagingSenderId: "896629947935",
   appId: "1:896629947935:web:d39368e812d9873ba91f2a",
 }
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
+/**
+ * QueryReference Objects do not hold the data, they have properties that tell us details about it,
+ * or the method to get the Snapshot Object which gives us the data we are looking for.
+ *
+ * QueryReference === Info
+ * QuerySnapshot === data
+ *
+ * CRUD (Create / Retrieve / Update / Delete) operations can be done only on the reference object
+ * not the snapshot.
+ *
+ * userAuth will return 'null' if you are not signedIn
+ *
+ * @param {*} userAuth
+ * @param {*} additionalData
+ */
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  // get the refernce of the data
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  // get the real data
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData })
+    } catch (error) {
+      console.log("error creating user", error.message)
+    }
+  }
+
+  // console.log(userRef)
+  return userRef
+}
+
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: "select_account" })
+
 export const signInWithGoogle = () =>
   auth.signInWithPopup(provider).catch((error) => {
     console.log(error)
   })
 
 export default firebase
+
+/**
+ * HOW TO ACCESS FIRESTORE DATA
+ *
+ * import firebase from "firebase/app"
+ * import "firebase/firestore"
+ *
+ * const firestore = firebase.firestore()
+ *
+ * There are 2 ways to access the data with chained .collection() & .doc() functions or
+ * with a complete path (string) inside a .collection() or .doc() function
+ * The next 2 lines mean the same
+ * firestore.collection("users").doc("ZCYotcmUQHAELJR5MFV7").collection("cartItems").doc("JA60QXUOrLeQtv0QKoZ4")
+ * firestore.doc("users/ZCYotcmUQHAELJR5MFV7/cartItems/JA60QXUOrLeQtv0QKoZ4")
+ *
+ * if you want a collection instead of a specifc item
+ * firestore.collection("users/ZCYotcmUQHAELJR5MFV7/cartItems")
+ */
